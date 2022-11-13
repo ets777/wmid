9b0ad50454d6b51da9440807ebbca6b2a69a3b75
ets777 <dmitry.klishev@gmail.com>
2022-11-13 16:48:14 +1000

    MPLU-71: calorie deficit calculation fixed

--- back/get_calorie_limit.php
+++ back/get_calorie_limit.php
@@ -7,16 +7,16 @@ $result = $mysqli->query("SELECT
 (SELECT value FROM params WHERE code = 'weight') weight,
 (SELECT value FROM params WHERE code = 'height') height,
 (SELECT value FROM params WHERE code = 'age') age,
-(SELECT value FROM params WHERE code = 'calorie_deficit_procent') calorie_deficit_procent");
+(SELECT value FROM params WHERE code = 'calorie_deficit') calorie_deficit");
 
 $row = $result->fetch_row();
 
 $weight = (int)$row[0];
 $height = (int)$row[1];
 $age = (int)$row[2];
-$calorie_deficit_procent = (float)$row[3];
+$calorie_deficit = (float)$row[3];
 
-$calorie_deficit = 1 - $calorie_deficit_procent / 100;
+$calorie_deficit = 1 - $calorie_deficit / 100;
 $calorie_limit = round($calorie_deficit * 1.2 * (10 * $weight + 6.25 * $height - 5 * $age + 5));
 
 echo json_encode($calorie_limit);
