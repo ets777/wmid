2a920a75b3f01ee32819f9025900634321aee917
ets777 <dmitry.klishev@gmail.com>
2023-06-01 17:30:30 +1000

    config fix

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -1,8 +1,11 @@
+#!/usr/bin/env node
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.enableCors();
   await app.listen(3000);
 }
--- src/backend/players/players.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class PlayersService {
-  getPlayers() {
-    return [{ name: 'Player One' }, { name: 'Player Two' }];
-  }
-}
