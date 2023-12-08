10d1cf6b951b0d77185662d14d0db0b8508587a9
ets777 <dmitry.klishev@gmail.com>
2023-12-08 21:41:03 +1000

    test for month tasks fix

--- src/database/test/appoint_random_task.sql
+++ src/database/test/appoint_random_task.sql
@@ -1,9 +1,19 @@
 -- Проверка назначения еженедельного задания в следующем году, но в ту же неделю по счёту (кейс 3.1)
 insert into tsk_tasks (id, text, nextTaskId, nextTaskBreak, startDate, endDate, categoryId, offset, duration, isActive, isDeleted, cooldown, isImportant, userId) values
-(36, 'Задание 36 (еженедельное)', null, null, '2002-01-01', '2003-01-01', 1, 1, 1, 1, 0, 0, 0, 1);
+(36, 'Задание 36 (еженедельное)', null, null, '2003-01-01', '2003-01-01', 1, 1, 1, 1, 0, 0, 0, 1);
 
 insert into tsk_periods (id, typeId, taskId, startTime, endTime, weekday, day, month, date) values
 (36, 2, 36, null, null, null, null, null, null);
 
 insert into tsk_appointments (id, startDate, endDate, statusId, taskId, isAdditional) values
 (17, '2002-01-01 06:00:00', '2002-01-01 06:01:00', 2, 36, 0);
+
+-- Проверка назначения ежемесячного задания в следующем году, но в тот же месяц (кейс 3.2)
+insert into tsk_tasks (id, text, nextTaskId, nextTaskBreak, startDate, endDate, categoryId, offset, duration, isActive, isDeleted, cooldown, isImportant, userId) values
+(37, 'Задание 37 (ежемесячное)', null, null, '2003-01-02', '2003-01-02', 1, 1, 1, 1, 0, 0, 0, 1);
+
+insert into tsk_periods (id, typeId, taskId, startTime, endTime, weekday, day, month, date) values
+(37, 3, 37, null, null, null, null, null, null);
+
+insert into tsk_appointments (id, startDate, endDate, statusId, taskId, isAdditional) values
+(18, '2002-01-02 06:00:00', '2002-01-02 06:01:00', 2, 37, 0);
